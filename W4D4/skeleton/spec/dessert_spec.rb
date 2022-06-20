dc256ac4d3fe52e4c79ebc4196cc9d62eaa2b816
“Jeahoung <jeahounghong@gmail.com>
2022-06-20 04:44:12 -0400

    W5D1

--- W4D4/skeleton/spec/dessert_spec.rb
+++ W4D4/skeleton/spec/dessert_spec.rb
@@ -9,21 +9,35 @@ describe Dessert do
   let(:chef) { double("chef") }
 
   describe "#initialize" do
-    it "sets a type"
+    subject(:dessert) {Dessert.new("cake", 50, "Gordon Ramsey")}
+    it "sets a type" do
+      expect(dessert.type).to eq("cake")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(50)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients.length).to eq(0)
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect(dessert.quantity).to be_a(Integer)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+        dessert.add_ingredient("sugar")
+        expect(dessert.ingredients[-1]).to eq("sugar")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+
+    end
   end
 
   describe "#eat" do
